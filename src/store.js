a8ad889e88c8286c0e71a6cd5b062d033c88b2cf
Paolo Falco <paolo.falcoapp@gmail.com>
2024-02-09 10:24:44 +0100

    mod to cart for restaurant

--- src/store.js
+++ src/store.js
@@ -8,21 +8,37 @@ const store = createStore({
   },
   mutations: {
     addToCart(state, dish) {
-      const existingDish = state.cart.find(item => item.id === dish.id);
-      if (existingDish) {
-        // Aggiorna la quantità del piatto esistente nel carrello
-        existingDish.quantity++;
-      } else {
-        // Controlla se un piatto con lo stesso nome è già nel carrello
-        const sameNamedDish = state.cart.find(item => item.name === dish.name);
-        if (sameNamedDish) {
-          // Aggiorna la quantità del piatto esistente con lo stesso nome nel carrello
-          sameNamedDish.quantity++;
+      // Verifica se il piatto appartiene al ristorante corrente
+      if (state.currentRestaurantId === dish.restaurant_id) {
+        const existingDish = state.cart.find(item => item.id === dish.id);
+        if (existingDish) {
+          // Aggiorna la quantità del piatto esistente nel carrello
+          existingDish.quantity++;
         } else {
-          // Se il piatto non esiste nel carrello, aggiungilo normalmente
-          dish.id = uuidv4();
-          dish.quantity = 1;
-          state.cart.push(dish);
+          // Controlla se un piatto con lo stesso nome è già nel carrello
+          const sameNamedDish = state.cart.find(item => item.name === dish.name);
+          if (sameNamedDish) {
+            // Aggiorna la quantità del piatto esistente con lo stesso nome nel carrello
+            sameNamedDish.quantity++;
+          } else {
+            // Se il piatto non esiste nel carrello, aggiungilo normalmente
+            dish.id = uuidv4();
+            dish.quantity = 1;
+            state.cart.push(dish);
+          }
+        } 
+      } else {
+        const confirmNewCart = window.confirm('Il piatto selezionato non appartiene al ristorante corrente. Vuoi creare un nuovo carrello?');
+        if (confirmNewCart) {
+        // Se il piatto non appartiene al ristorante corrente, resetta il carrello e aggiungi il nuovo piatto
+        state.cart = [];
+        dish.id = uuidv4();
+        dish.quantity = 1;
+        state.cart.push(dish);
+        state.currentRestaurantId = dish.restaurant_id;
+        } else {
+          // Non aggiungere il piatto al carrello
+          return;
         }
       }
     },
